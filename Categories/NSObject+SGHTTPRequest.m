c35065c34399710cfda29d5b41046ae324d2fd4e
James Van-As <james.vanas@gmail.com>
2015-10-19 14:19:45 +0100

    Big speedup of the null cleansing routine

--- Categories/NSObject+SGHTTPRequest.m
+++ Categories/NSObject+SGHTTPRequest.m
@@ -16,16 +16,14 @@
 }
 
 + (id)sghttp_nullCleanse:(id)obj
-                      loggingURL:(NSString *)loggingURL
-                       logString:(NSMutableString *)logString
-                        nullPath:(NSString *)nullPath {
+              loggingURL:(NSString *)loggingURL
+               logString:(NSMutableString *)logString
+                nullPath:(NSString *)nullPath {
     if ([obj isKindOfClass:NSDictionary.class]) {
         NSDictionary *dict = obj;
-        NSMutableDictionary *cleansed = dict.mutableCopy;
+        NSMutableDictionary *cleansed = NSMutableDictionary.new;
         for (id key in dict) {
             if ([dict[key] isKindOfClass:NSNull.class]) {
-                [cleansed removeObjectForKey:key];
-
                 if (SGHTTPRequest.logging & SGHTTPLogNullCleanses) {
                     if (!logString.length) {
                         if (loggingURL.length) {
@@ -44,6 +42,8 @@
                                                    logString:logString
                                                     nullPath:nullPath ? [nullPath stringByAppendingFormat:@"[@\"%@\"]", key] :
                                  [NSString stringWithFormat:@"[@\"%@\"]", key]];
+            } else {
+                cleansed[key] = dict[key];
             }
         }
         if (!nullPath && [logString length]) {
@@ -53,11 +53,11 @@
         return cleansed;
     } else if ([obj isKindOfClass:NSArray.class]) {
         NSArray *array = obj;
-        NSMutableArray *cleansed = array.mutableCopy;
-        for (id item in array) {
-            if ([item isKindOfClass:NSNull.class]) {
-                [cleansed removeObject:item];
+        NSMutableArray *cleansed = NSMutableArray.new;
 
+        for (NSUInteger i = 0; i < array.count; i++) {
+            id item = array[i];
+            if ([item isKindOfClass:NSNull.class]) {
                 if (SGHTTPRequest.logging & SGHTTPLogNullCleanses) {
                     if (![logString length]) {
                         if (loggingURL.length) {
@@ -75,8 +75,10 @@
                                                     loggingURL:loggingURL
                                                      logString:logString
                                                       nullPath:nullPath ? [nullPath stringByAppendingString:@"{}"] :
-                                                                                                            @"{}"];
-                [cleansed replaceObjectAtIndex:[cleansed indexOfObject:item] withObject:cleansedItem];
+                                   @"{}"];
+                [cleansed addObject:cleansedItem];
+            } else {
+                [cleansed addObject:item];
             }
         }
         if (!nullPath && [logString length]) {
