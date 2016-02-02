aad7ef56378f06c7d351b22fe1591dbef057e8f4
James Van-As <james.vanas@gmail.com>
2016-02-02 22:53:07 +0000

    Changes to travis.yml file

---
+++ Example/Pods/Headers/Public/SGHTTPRequest/SGHTTPRequestDebug.h
@@ -0,0 +1,1 @@
+../../../../../SGHTTPRequest/Debug/SGHTTPRequestDebug.h
--- Example/Pods/Target Support Files/Pods-SGHTTPRequest_Tests/Pods-SGHTTPRequest_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-SGHTTPRequest_Tests/Pods-SGHTTPRequest_Tests-frameworks.sh
@@ -82,12 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-SGHTTPRequest_Tests/AFNetworking.framework"
-  install_framework "Pods-SGHTTPRequest_Tests/SGHTTPRequest.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-SGHTTPRequest_Tests/AFNetworking.framework"
-  install_framework "Pods-SGHTTPRequest_Tests/SGHTTPRequest.framework"
-fi
